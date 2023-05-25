59ef913314af32b92105d32c4cd4f00a3b5aba60
Levylubinda <lubasi223@gmail.com>
2023-05-25 13:40:39 +0000

    Upadte

---
+++ models/review.py
@@ -0,0 +1,11 @@
+ass review that inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represents a class Review
+    """
+    place_id = ''
+    user_id = ''
+    text = ''
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""A class State that inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents a class State
+    """
+
+
